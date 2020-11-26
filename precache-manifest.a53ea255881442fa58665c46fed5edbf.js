c6e5d51901d04a44838f181fb1d9133ea5288e73
daonghia2404 <daonghia2404@gmail.com>
2020-11-26 11:32:01 +0700

    Updates

---
+++ precache-manifest.a53ea255881442fa58665c46fed5edbf.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be8ad654d1c33d94d833bb5fa85ec6f",
+    "url": "/MoveekApp/index.html"
+  },
+  {
+    "revision": "67c8882abd30768db583",
+    "url": "/MoveekApp/static/css/2.68d1933a.chunk.css"
+  },
+  {
+    "revision": "b20e76c441f7b1664079",
+    "url": "/MoveekApp/static/css/main.d36304e4.chunk.css"
+  },
+  {
+    "revision": "67c8882abd30768db583",
+    "url": "/MoveekApp/static/js/2.872e5f44.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MoveekApp/static/js/2.872e5f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20e76c441f7b1664079",
+    "url": "/MoveekApp/static/js/main.43cdaa9e.chunk.js"
+  },
+  {
+    "revision": "3e86276f21fcd00de55a",
+    "url": "/MoveekApp/static/js/runtime-main.7800757a.js"
+  },
+  {
+    "revision": "a61d19a0ce67eb09e7e670fe48506b8f",
+    "url": "/MoveekApp/static/media/avatar-user.a61d19a0.png"
+  },
+  {
+    "revision": "97990141741c72b3225032ddc62e2fdc",
+    "url": "/MoveekApp/static/media/background-banner.97990141.jpg"
+  },
+  {
+    "revision": "c29c642e50c29a96f148c5337057434a",
+    "url": "/MoveekApp/static/media/background-overlay.c29c642e.png"
+  },
+  {
+    "revision": "2f4666f2a2752942f0f8fd6a429a8d0c",
+    "url": "/MoveekApp/static/media/background-section-2.2f4666f2.svg"
+  },
+  {
+    "revision": "09ab2418cc8a6dd596e275f1c0d79eb1",
+    "url": "/MoveekApp/static/media/icon-calendar.09ab2418.svg"
+  },
+  {
+    "revision": "e30eccc7d3ffb104a61390904d9ce4ba",
+    "url": "/MoveekApp/static/media/icon-clock.e30eccc7.svg"
+  },
+  {
+    "revision": "f9e9bb5d7c5da8fd9923bd5fcbbe1719",
+    "url": "/MoveekApp/static/media/icon-country.f9e9bb5d.svg"
+  },
+  {
+    "revision": "3654ed9fe3f2f08cee199d6de023687b",
+    "url": "/MoveekApp/static/media/icon-grid.3654ed9f.svg"
+  },
+  {
+    "revision": "bb4e653583b6b1ab381f602258e9017d",
+    "url": "/MoveekApp/static/media/icon-peoples.bb4e6535.svg"
+  },
+  {
+    "revision": "0105da3582c26bc3fb85381d4844fc23",
+    "url": "/MoveekApp/static/media/icon-popularity.0105da35.svg"
+  },
+  {
+    "revision": "57e7de00351612bfa43cbf57bac056a5",
+    "url": "/MoveekApp/static/media/icon-price.57e7de00.svg"
+  },
+  {
+    "revision": "3d49571cde353fc480217ce5f43d23a7",
+    "url": "/MoveekApp/static/media/icon-search.3d49571c.svg"
+  },
+  {
+    "revision": "bbb7ece818f3e9db0d35716fcfc47147",
+    "url": "/MoveekApp/static/media/icon-star-fill.bbb7ece8.svg"
+  },
+  {
+    "revision": "a210c76c4eaa43005fe1172e6967e63f",
+    "url": "/MoveekApp/static/media/icon-star-half.a210c76c.svg"
+  },
+  {
+    "revision": "57cc434e087b750ecf2cd6906b9966dd",
+    "url": "/MoveekApp/static/media/icon-star.57cc434e.svg"
+  },
+  {
+    "revision": "33e36cd464d712215613735d1119ff31",
+    "url": "/MoveekApp/static/media/icon-vote.33e36cd4.svg"
+  },
+  {
+    "revision": "7c491d5d194c037a0028a1a55d5109e2",
+    "url": "/MoveekApp/static/media/icon-web.7c491d5d.svg"
+  },
+  {
+    "revision": "25664ec3975530d3a9de4293d68ac131",
+    "url": "/MoveekApp/static/media/imdb.25664ec3.png"
+  },
+  {
+    "revision": "2d8012e1190ab19eacaa628d58110375",
+    "url": "/MoveekApp/static/media/logo-panel.2d8012e1.png"
+  },
+  {
+    "revision": "870f4e23ee85180a8a0ed2641a5fe3cc",
+    "url": "/MoveekApp/static/media/netflix-logo.870f4e23.png"
+  },
+  {
+    "revision": "222e7c1d77fc7aad65a9cc31146f716a",
+    "url": "/MoveekApp/static/media/poster-mockup.222e7c1d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daonghia2404/MoveekApp/precache-manifest.2423e0d50e5b01a3454ea1f83cf99094.js"
+  "/MoveekApp/precache-manifest.a53ea255881442fa58665c46fed5edbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daonghia2404/MoveekApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MoveekApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
